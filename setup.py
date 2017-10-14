8bdadf53238aac594e70346efccad1be09434560
molinge <molingejr@gmail.com>
2017-10-14 04:09:06 +0100

    Updating.......

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='operator_expression_generator',
     version='1.0',
-    packages=[''],
-    url='',
+
+    # This automatically detects the packages in the specified
+    # (or current directory if no directory is given).
+    packages=find_packages(exclude=['docs']),
+
     license='',
+
+    # All of the following fields are PyPI metadata fields.
+    # When registering a package at PyPI this is used as
+    # information on the package page.
     author='Molinge',
     author_email='molingejr@gmail.com',
-    description='Operator and Expression Generator'
+
+    description='An Operator and Expression Generator',
+    long_description=""" """,
+    install_requires=['PyQt5', 'pickle', 'gzip'],
+    # setup_requires=['PyQt5'],
+    package_data={
+        # Include (markdown text) documentation files from any directory
+        '': ['*.md']
+    },
+
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+
+    ]
 )
