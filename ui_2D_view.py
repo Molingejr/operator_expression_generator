da97e469c3453127b2ef968b4f58d9d667196e91
molinge <molingejr@gmail.com>
2017-10-22 10:18:52 +0100

    albeit of tweaking

--- ui_2D_view.py
+++ ui_2D_view.py
@@ -11,9 +11,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog_2D(object):
     def setupUi(self, Dialog_2D):
         Dialog_2D.setObjectName("Dialog_2D")
+        Dialog_2D.setWindowModality(QtCore.Qt.NonModal)
+        Dialog_2D.setEnabled(True)
         Dialog_2D.resize(400, 230)
+        Dialog_2D.setMinimumSize(QtCore.QSize(400, 220))
+        Dialog_2D.setSizeIncrement(QtCore.QSize(0, 0))
+        Dialog_2D.setAcceptDrops(False)
+        Dialog_2D.setSizeGripEnabled(False)
+        Dialog_2D.setModal(False)
         self.tableWidget = QtWidgets.QTableWidget(Dialog_2D)
         self.tableWidget.setGeometry(QtCore.QRect(20, 10, 211, 192))
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setDragEnabled(True)
+        self.tableWidget.setDragDropMode(QtWidgets.QAbstractItemView.DragOnly)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
@@ -29,6 +39,7 @@ class Ui_Dialog_2D(object):
         self.pushButton_cancel.setObjectName("pushButton_cancel")
 
         self.retranslateUi(Dialog_2D)
+        self.pushButton_cancel.clicked.connect(Dialog_2D.close)
         QtCore.QMetaObject.connectSlotsByName(Dialog_2D)
 
     def retranslateUi(self, Dialog_2D):
